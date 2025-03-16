0a9927d049b22138488152c7eabe9a674a3d0157
lonchanick <diego.arroyo2462050@gmail.com>
2025-03-16 15:39:40 -0300

    ...

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const kit_dpa = document.getElementById('kit_dpa');
-const litros_agua = document.getElementById('litros_agua');
+const kit_dpaE = document.getElementById('kit_dpa');
+const litros_aguaE = document.getElementById('litros_agua');
 const resultContainer = document.getElementById('result');
+const fruta_opcionE = document.getElementById('option');
+let ppm_fruta = 1500;
 
-
+fruta_opcionE.addEventListener('change',()=>{
+    if(fruta_opcionE.value === 'manzana')
+        ppm_fruta =1700;
+    else if(fruta_opcionE.value === 'pera')
+        ppm_fruta = 1500;
+});
 
 function calculate()
 {
-    const parse_kit_dpa = parseFloat(kit_dpa.value);
-    const parse_litros_agua = parseFloat(litros_agua.value);
+    const kit_dpa = parseFloat(kit_dpaE.value);
+    const litros_agua = parseFloat(litros_aguaE.value);
     resultContainer.innerHTML += 
-    `<p>Litros de DPA que actualmente tiene el tanque: ${((parse_kit_dpa * parse_litros_agua)/310000).toFixed(2)}</p>
-    <p>Degradacion: ${1500-parse_kit_dpa}</p>
-    <p>DPA recarga: ${((1500-parse_kit_dpa)*4000/310000).toFixed(2)}</p>`;
+    `<p class="mb-0">Cantidad DPA en tanque: ${((kit_dpa * litros_agua)/310000).toFixed(2)} Litros.</p>
+    <p class="mb-0">Degradacion: ${ppm_fruta-kit_dpa}PPM -${100 - ((kit_dpa*100) / ppm_fruta).toFixed(2)}%</p>
+    <p class="mb-0">DPA a recargar: ${((ppm_fruta-kit_dpa)*4000/310000).toFixed(2)} Litros.</p>`;
 }
